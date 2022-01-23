898a68d1a0a9b524ebeea69da641075bc4b3e365
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2022-01-23 23:25:50 +0700

    fix minor typo in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 moralis-admin-cli deploy \
 --moralisApiKey ${MORALIS_API_KEY} \
 --moralisApiSecret ${MORALIS_API_SECRET} \
---moralisSubdomain ${MORALIS_API_SUBDOMAIN}
+--moralisSubdomain ${MORALIS_API_SUBDOMAIN} \
 --folderPath ${FOLDER_PATH:=./}
