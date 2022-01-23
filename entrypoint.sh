d0b58d3791221ef22908a82ee657750b748e12e2
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2022-01-23 22:05:20 +0700

    change subdomain script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 npx moralis-admin-cli deploy \
 --moralisApiKey ${MORALIS_API_KEY} \
 --moralisApiSecret ${MORALIS_API_SECRET} \
---moralisApiSubdomain ${MORALIS_API_SUBDOMAIN}
+--moralisSubdomain ${MORALIS_API_SUBDOMAIN}
