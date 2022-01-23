574a560edfd478c97db931531d63e491dcaa99be
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2022-01-23 21:55:22 +0700

    include the keys in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -l
 
-npx moralis-admin-cli deploy
+npx moralis-admin-cli deploy \
+--moralisApiKey ${MORALIS_API_KEY} \
+--moralisApiSecret ${MORALIS_API_SECRET} \
+--moralisApiSubdomain ${MORALIS_API_SUBDOMAIN}
